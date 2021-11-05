0d51f1c0bb376cdbc29dde6ff40e9fb9562a336a
Rogelio Torres <torresroger445@gmail.com>
2021-11-05 12:03:12 -0600

    config database

--- config/database.js
+++ config/database.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
-  defaultConnection: "default",
+  defaultConnection: 'default',
   connections: {
     default: {
-      connector: "bookshelf",
+      connector: 'bookshelf',
       settings: {
         client: "postgres",
         host: env("DATABASE_HOST", "ec2-34-226-18-183.compute-1.amazonaws.com"),
@@ -11,6 +11,9 @@ module.exports = ({ env }) => ({
         username: env("DATABASE_USERNAME", "rkkqlwcyecstku"),
         password: env("DATABASE_PASSWORD", "d75781cf403172aebb8b67aa15922ff7b9e9bd5198e2068f9fd04ca40b37b8be"),
         schema: env("DATABASE_SCHEMA", "public"),
+        ssl: {
+          rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+        },
       },
       options: {
         ssl: env.bool('DATABASE_SSL', false),
