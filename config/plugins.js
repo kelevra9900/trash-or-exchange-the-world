c9b024a6cb1239b34b6e2a167e0a4b8ead1d2757
Rogelio Torres <torresroger445@gmail.com>
2021-11-14 18:48:37 -0600

    logo in categories

--- config/plugins.js
+++ config/plugins.js
@@ -1,13 +1,13 @@
 module.exports = ({ env }) => ({
-    upload: {
-      provider: 'aws-s3',
-      providerOptions: {
-        accessKeyId: env('AWS_ACCESS_KEY_ID', 'AKIAYOH3KIC7KX44VQ5J'),
-        secretAccessKey: env('AWS_ACCESS_SECRET', '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'),
-        region: 'us-east-2',
-        params: {
-          Bucket: 'trash-or',
-        },
+  upload: {
+    provider: 'aws-s3',
+    providerOptions: {
+      accessKeyId: env('AWS_ACCESS_KEY_ID', 'AKIAZK6ADPVBRNF4CVXO'),
+      secretAccessKey: env('AWS_ACCESS_SECRET', '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'),
+      region: 'us-east-2',
+      params: {
+        Bucket: 'seguridad-en-america',
       },
-    }
-  });
+    },
+  }
+});
